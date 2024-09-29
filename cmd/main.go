b8e10fd3da60094744a49cca3c1872863cf91822
Fairuzzzzz <fairuz.afnan@gmail.com>
2024-09-29 13:16:43 +0700

    add config

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"log"
+
+	"github.com/Fairuzzzzz/simpleform/internal/configs"
 	"github.com/Fairuzzzzz/simpleform/internal/handler/membership"
 	"github.com/gin-gonic/gin"
 )
@@ -8,7 +11,24 @@ import (
 func main() {
 	r := gin.Default()
 
+	var (
+		cfg *configs.Config
+	)
+
+	err := configs.Init(
+		configs.WithConfigFolder([]string{"./internal/configs/"}),
+		configs.WithConfigFile("config"),
+		configs.WithConfigType("yaml"),
+	)
+
+	if err != nil {
+		log.Fatal("Gagal inisialisasi config", err)
+	}
+
+	cfg = configs.Get()
+	log.Println("config", cfg)
+
 	membershipHandler := membership.NewHandler(r)
 	membershipHandler.RegisterRoute()
-	r.Run(":8000")
+	r.Run(cfg.Service.Port)
 }
